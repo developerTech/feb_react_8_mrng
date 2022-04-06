f37efeaf09bc4a4abf72e7d90a86ef034336f1b9
developerfunnel@gmail.com <ahanda205@gmail.com>
2022-04-06 08:57:07 +0530

    reactnative

--- mymobi/App.js
+++ mymobi/App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View, Alert ,Button } from 'react-native';
+import DisplayComponent from './component/displayComponent';
+
+const url = "https://zomatoajulypi.herokuapp.com/restaurant"
 
 export default function App() {
+
+  const [title,setTitle] = useState('Developer Button Clicked')
+  const [rest,setRest] = useState();
+
+  useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setRest(data)
+    })
+  },[])
+
+  const updateTitle = () => {
+    setTitle('New Title Added')
+  }
+
   return (
     <View style={styles.container}>
       <Text>React Native App</Text>
-      <StatusBar style="auto" />
+      <Text>{title}</Text>
+      <Button title="Click Me" onPress={updateTitle} />
+      <Button title="Revert Me" onPress={() => {setTitle('Developer Button Clicked')}} />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <DisplayComponent restList={rest}/>
     </View>
   );
 }
